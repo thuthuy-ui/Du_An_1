191e50d0017865a332f838e565cf18388f605188
VanHuy404 <nvhuydep@gmail.com>
2025-07-12 23:02:48 +0700

    Qllv
    ádhdgđ

---
+++ QuanLyLinhVucController.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package poly.books.controller;
+
+import poly.books.entity.LinhVuc;
+
+/**
+ *
+ * @author HuyNguyen
+ */
+public interface QuanLyLinhVucController extends CrudController<LinhVuc>{
+    
+}
